da450002db45359aedd6eb4a24626660eec803e7
Absa <luisopazo@gmail.com>
2024-06-16 16:10:14 -0400

    Update calendar-teams-poc.py

--- calendar-teams-poc.py
+++ calendar-teams-poc.py
@@ -1,9 +1,12 @@
 import msal
 import requests
+import sys
+import json
 
 client_id = 'xx'
 client_secret = 'xx'
 tenant_id = 'xx'
+teamID = "xx"
 
 # Crear una instancia de la aplicación de cliente pública
 app = msal.PublicClientApplication(client_id, authority=f"https://login.microsoftonline.com/{tenant_id}")
@@ -18,13 +21,15 @@ print(flow["message"])  # Muestra el mensaje al usuario para iniciar sesión
 
 # Esperar a que el usuario complete el flujo
 result = app.acquire_token_by_device_flow(flow)
-print(result)
+# print(result)
 if "access_token" in result:
     access_token = result["access_token"]
     print("Token de acceso obtenido con éxito.")
 else:
     print("Error al obtener el token de acceso.")
     print(result)
+    sys.exit(0)
+
 
 # authority = f'https://login.microsoftonline.com/{tenant_id}'
 # scopes = ['https://graph.microsoft.com/.default']
@@ -43,66 +48,101 @@ else:
 #     print(result)
 
 
-team_name = 'Subgerencia Servicios TI'
-url = f'https://graph.microsoft.com/v1.0/groups?$filter=displayName eq \'{team_name}\''
+# ----------------------------
+# --- Obtener ID de Teams
+# ----------------------------
+
+# team_name = 'Subgerencia Servicios TI'
+# url = f'https://graph.microsoft.com/v1.0/groups?$filter=displayName eq \'{team_name}\''
+# headers = {
+#     'Authorization': f'Bearer {access_token}'
+# }
+
+# response = requests.get(url, headers=headers)
+
+# group_id = ""
+
+# if response.status_code == 200:
+#     groups = response.json()
+#     if groups['value']:
+#         group_id = groups['value'][0]['id']
+#         print(f"ID del grupo: {group_id}")
+#     else:
+#         print("No se encontró el grupo.")
+# else:
+#     print(f"Error al obtener el ID del grupo: {response.status_code}")
+#     print(response.json())
+
+# headers = {
+#     'Authorization': f'Bearer {access_token}',
+#     'Content-Type': 'application/json'
+# }
+
+# ----------------------------
+# --- Obtener los canales del equipo
+# ----------------------------
+
+# response = requests.get(f'https://graph.microsoft.com/v1.0/teams/{group_id}/channels', headers=headers)
+
+# if response.status_code == 200:
+#     channels = response.json()
+#     for channel in channels['value']:
+#         print(f"Canal: {channel['displayName']} - ID: {channel['id']}")
+# else:
+#     print(f"Error al obtener los canales: {response.status_code}")
+#     print(response.json())
+
+# calendar_id = '19:0454537ce3ae4e7dbc94aeca919da2ae@thread.tacv2'  # El ID del calendario de Teams
+
+
+# ID del Team 'Subgerencia Servicios TI'
+
+
+url = f'https://graph.microsoft.com/v1.0/groups/{teamID}/events'
+
 headers = {
     'Authorization': f'Bearer {access_token}'
 }
 
 response = requests.get(url, headers=headers)
 
-group_id = ""
+# print(response.json())
 
-if response.status_code == 200:
-    groups = response.json()
-    if groups['value']:
-        group_id = groups['value'][0]['id']
-        print(f"ID del grupo: {group_id}")
-    else:
-        print("No se encontró el grupo.")
-else:
-    print(f"Error al obtener el ID del grupo: {response.status_code}")
-    print(response.json())
+with open('calendrio-gestion-cambio.csv', 'w') as file:
+    while True:
+        if response.status_code == 200:
+            events = response.json()
+            json_pretty = json.dumps(events, indent=4)
+            # print(json_pretty)
+            # if True:
+            #     with open('example.json', 'w') as file:
+            #         file.write(json_pretty)
+            #     sys.exit(0)
+            for event in events['value']:
+                file.write(event['subject']+";")
+                file.write(event['start']['dateTime']+";")
+                file.write(event['end']['dateTime']+";")
+                try:
+                    attendees = event['attendees']
+                    if nextLink is not None:
+                        for attendee in event['attendees']:
+                            file.write(attendee['emailAddress']['address']+";")
+                        file.write("\n")
+                except:
+                    file.write("\n")
+                    continue
+            try:
+                nextLink = events['@odata.nextLink']
+            except Exception as e:
+                print(f"Ocurrió un error: {e}")
+                sys.exit(0)
 
-headers = {
-    'Authorization': f'Bearer {access_token}',
-    'Content-Type': 'application/json'
-}
+            if nextLink is None:
+                break
+            else:
+                response = requests.get(nextLink, headers=headers)
+            
 
-# Obtener los canales del equipo
-response = requests.get(f'https://graph.microsoft.com/v1.0/teams/{group_id}/channels', headers=headers)
-
-if response.status_code == 200:
-    channels = response.json()
-    for channel in channels['value']:
-        print(f"Canal: {channel['displayName']} - ID: {channel['id']}")
-else:
-    print(f"Error al obtener los canales: {response.status_code}")
-    print(response.json())
-
-
-url = f'https://graph.microsoft.com/v1.0/groups/xx/events'
-headers = {
-    'Authorization': f'Bearer {access_token}'
-}
-
-response = requests.get(url, headers=headers)
-
-print(response.json())
-
-while True:
-    if response.status_code == 200:
-        events = response.json()
-        for event in events['value']:
-            print(f"Evento: {event['subject']}")
-            print(f"Inicio: {event['start']['dateTime']}")
-            print(f"Fin: {event['end']['dateTime']}")
-            print("-" * 20)
-        nextLink = events['@odata.nextLink']
-        if nextLink is None:
-            break
         else:
-            response = requests.get(nextLink, headers=headers)
-    else:
-        print(f"Error al obtener los eventos: {response.status_code}")
-        print(response.json())
+            print(f"Error al obtener los eventos: {response.status_code}")
+            print(response.json())
